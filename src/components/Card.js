083deaecea4a2a05c6d729fab1e0e25537e80209
FrancoOrtiz <francoportiz@gmail.com>
2022-03-28 17:23:44 -0300

    Created Card component

---
+++ src/components/Card.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export const Card = () => {
+  return (
+    <div className="flex w-80 h-40 justify-between my-10">
+      <div className="h-full w-40 bg-gray-500">
+        <img src=""></img>
+      </div>
+      <span className="mt-2 text-xl text-gray-500">Pikachu</span>
+    </div>
+  )
+}
--- src/components/Search.js
+++ src/components/Search.js
@@ -1,22 +1,14 @@
 import React from 'react'
+import { Card } from './Card'
 
 export const Search = () => {
   return (
-    <main>
-        <h2>Resultados de la búsqueda</h2>
-        <div>
-            <div>
-                <img></img>
-                <span></span>
-            </div>
-            <div>
-                <img></img>
-                <span></span>
-            </div>
-            <div>
-                <img></img>
-                <span></span>
-            </div>
+    <main style={{minHeight: "calc(100vh - 80px - 200px)"}} className="w-4/5 m-auto mt-4">
+        <h2 className="text-black font-bold text-4xl">Resultados de la búsqueda</h2>
+        <div className="mt-10">
+            <Card></Card>
+            <Card></Card>
+            <Card></Card>
         </div>
     </main>
   )
