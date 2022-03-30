58934449b7a162b4406f643bdda5dccbdb8964c3
FrancoOrtiz <francoportiz@gmail.com>
2022-03-30 15:53:09 -0300

    Extracting and applying data from Context

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,12 +1,13 @@
-import React from 'react'
+import React from 'react';
 
-export const Card = () => {
+export const Card = ({pokemonName, pokemonImg}) => {
+
   return (
-    <div className="flex w-80 h-40 justify-between my-10">
+    <div className="flex w-80 h-40 my-10">
       <div className="h-full w-40 bg-gray-500">
-        <img src=""></img>
+        <img className="w-full" src={pokemonImg} alt=""></img>
       </div>
-      <span className="mt-2 text-xl text-gray-500">Pikachu</span>
+      <span className="mt-4 ml-10 text-xl font-bold text-gray-400">{pokemonName}</span>
     </div>
   )
 }
