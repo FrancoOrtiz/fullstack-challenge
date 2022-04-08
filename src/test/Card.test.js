ae922a76f871f172b1c78f97db38e90fb0be8a2f
FrancoOrtiz <francoportiz@gmail.com>
2022-04-08 15:23:50 -0300

    Added toMatchSnapshot

--- src/test/Card.test.js
+++ src/test/Card.test.js
@@ -1,19 +1,37 @@
+/* eslint-disable testing-library/prefer-screen-queries */
 import React from "react";
 import { Card } from "../components/Card.js";
+import renderer from "react-test-renderer";
 import { render } from "@testing-library/react";
 
-describe("", () => {
+describe("Tests on Card.js", () => {
+  let wrapper;
+
+  const Info = {
+    pokemonName: "pikachu",
+    pokemonImg:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/25.png",
+    pokemonNum: 35,
+  };
+
+  const { pokemonName, pokemonImg, pokemonNum } = Info;
+
+  test("should be render", () => {
+    wrapper = renderer
+      .create(
+        <Card
+          pokemonName={pokemonName}
+          pokemonImg={pokemonImg}
+          pokemonNum={pokemonNum}
+        />
+      )
+      .toJSON();
+
+    expect(wrapper).toMatchSnapshot();
+  });
+
   test("should be display correctly", () => {
-    const Info = {
-      pokemonName: "pikachu",
-      pokemonImg:
-        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/25.png",
-      pokemonNum: 35,
-    };
-
-    const { pokemonName, pokemonImg, pokemonNum } = Info;
-
-    const wrapper = render(
+    wrapper = render(
       <Card
         pokemonName={pokemonName}
         pokemonImg={pokemonImg}
@@ -22,7 +40,9 @@ describe("", () => {
     );
 
     wrapper.getByText("pikachu");
-    const img = wrapper.container.querySelector("img").src;
+    wrapper.getByText(35);
+
+    const img = wrapper.getByRole("img").src;
 
     expect(img).toEqual(
       "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/25.png"
